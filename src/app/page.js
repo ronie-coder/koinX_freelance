0b92c60713db6d9604869b252166555ac897b49a
ronie-coder <ronweasly9435@gmail.com>
2024-03-01 11:06:33 +0530

    initializing and dimensioning the bottom component 'YouMayAlsoLike'

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@
 import Image from "next/image";
 import PageDirectoryShow from "@/components/page directory/PageDirectoryShow";
 import GraphTrendingContain from "@/components/Graph and trending Container/GraphTrendingContain";
+import YouMayAlsoLike from "@/components/YouMayAlsoLike/YouMayAlsoLike";
 export default function Home() {
   return (
     <div className=" max-w-[1440px] whitespace-nowrap h-[1629px]  relative  max-[411px]:h-[1800px]">
     <PageDirectoryShow></PageDirectoryShow>
     <GraphTrendingContain></GraphTrendingContain>
+    <YouMayAlsoLike></YouMayAlsoLike>
    </div>
   );
 }
---
+++ src/components/YouMayAlsoLike/YouMayAlsoLike.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const YouMayAlsoLike = () => {
+  return (
+    <div className="w-[100vw] h-[628px] bg-white absolute left-[50%] translate-x-[-50%] bottom-0 flex items-center justify-center max-[411px]:w-[384px] max-[411px]:h-[510px] max-[411px]:top-[600px]">
+      <div className="w-[1440px] h-[492px]  flex flex-col relative max-[411px]:w-[384px] bg-white">
+      
+      </div>
+      </div>
+  )
+}
+
+export default YouMayAlsoLike
