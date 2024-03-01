95d89938aa7f845593aae9cbf41d1395aa104868
ronie-coder <ronweasly9435@gmail.com>
2024-03-01 10:34:04 +0530

    pageDirectory_Component_Completion

---
+++ src/components/page directory/PageDirectoryShow.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageDirectoryShow = () => {
+  return (
+    <div className="mt-2 mb-3 max-[411px]:px-5">
+        <span className=" pr-3 text-[11px] text-slate-500">Cryptocurrencies</span>
+        <span className=" pr-3 text-[11px] text-slate-500">&gt;&gt;</span>
+        <span className=" pr-3 text-[11px] font-bold">Bitcoin</span>
+    </div>
+  )
+}
+
+export default PageDirectoryShow
