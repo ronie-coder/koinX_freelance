b3a651f91980bdbcf70899f07dd4d4d208aa734e
ronie-coder <ronweasly9435@gmail.com>
2024-03-01 10:22:56 +0530

    fetching all trending coins data and storing it in usestate

--- src/context/coinsContext.jsx
+++ src/context/coinsContext.jsx
@@ -1,21 +1,29 @@
-"use client"
+"use client";
 
 import { createContext } from "react";
 
-
-
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const coinContext = createContext({});
 
-export const CoinContextProvider = ({children})=>{
-    
-   
-    
-    return (
-        <coinContext.Provider>
-            {children}
-        </coinContext.Provider>
-    )
-}
+export const CoinContextProvider = ({ children }) => {
+  const [trendingCoins, setTrendingCoins] = useState([]);
+
+  useEffect(() => {
+    try {
+      const fetchAll = async () => {
+        const res = await axios.get(
+          "https://api.coingecko.com/api/v3/search/trending",
+          { crossDomain: true }
+        );
+        setTrendingCoins(res.data);
+      };
+      fetchAll();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [axios]);
+
+  return <coinContext.Provider>{children}</coinContext.Provider>;
+};
