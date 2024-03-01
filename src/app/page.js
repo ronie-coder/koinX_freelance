b2ae549e9ff3f5efe08b0ccb5652991cad3f9691
ronie-coder <ronweasly9435@gmail.com>
2024-03-01 10:31:53 +0530

    assigning dimensions_toMainPage_accToFigma

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <></>
+    <div className=" max-w-[1440px] whitespace-nowrap h-[1629px]  relative  max-[411px]:h-[1800px]">
+   
+   </div>
   );
 }
