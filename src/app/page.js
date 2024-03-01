01dc5b57739e1f37d38769df08658cdf7e9bbea4
ronie-coder <ronweasly9435@gmail.com>
2024-03-01 10:38:53 +0530

    HeroGraph & Trending Container Dimensions assigning

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
-
+import PageDirectoryShow from "@/components/page directory/PageDirectoryShow";
 export default function Home() {
   return (
     <div className=" max-w-[1440px] whitespace-nowrap h-[1629px]  relative  max-[411px]:h-[1800px]">
-   
+    <PageDirectoryShow></PageDirectoryShow>
    </div>
   );
 }
---
+++ src/components/Graph and trending Container/GraphTrendingContain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GraphTrendingContain = () => {
+  return (
+    <div className="w-full h-[760px]  flex justify-between gap-[20px] max-[411px]:flex-col max-[411px]:h-[1000px]">
+       
+    </div>
+  )
+}
+
+export default GraphTrendingContain
