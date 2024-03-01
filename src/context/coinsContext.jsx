3ce51961b37b11d8e03318b446d88693e886d451
ronie-coder <ronweasly9435@gmail.com>
2024-03-01 10:25:23 +0530

    fetching bitcoin price in usd and inr_Storing in UseState

--- src/context/coinsContext.jsx
+++ src/context/coinsContext.jsx
@@ -9,6 +9,8 @@ export const coinContext = createContext({});
 
 export const CoinContextProvider = ({ children }) => {
   const [trendingCoins, setTrendingCoins] = useState([]);
+  const [priceUSD, setPriceUSD] = useState({});
+  const [priceINR, setPriceINR] = useState({});
 
   useEffect(() => {
     try {
@@ -18,6 +20,35 @@ export const CoinContextProvider = ({ children }) => {
           { crossDomain: true }
         );
         setTrendingCoins(res.data);
+      };
+      fetchAll();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [axios]);
+
+  useEffect(() => {
+    try {
+      const fetchAll = async () => {
+        const res = await axios.get(
+          "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
+          { crossDomain: true }
+        );
+        setPriceUSD(res.data);
+      };
+      fetchAll();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [axios]);
+  useEffect(() => {
+    try {
+      const fetchAll = async () => {
+        const res = await axios.get(
+          "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr",
+          { crossDomain: true }
+        );
+        setPriceINR(res.data);
       };
       fetchAll();
     } catch (error) {
